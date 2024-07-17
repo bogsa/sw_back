fc0046819be0f7f2c9dbe04c48c79a841399207a
stillrap84 <stillrap@gmail.com>
2024-07-17 17:41:33 -0600

    R-17072024-001

--- sw.Datos/ApplicationDbContext.cs
+++ sw.Datos/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ using sw.Datos.Administracion.TFDs;
 
 using sw.Datos.Configuracion.CatalogoSAT;
 using sw.Datos.Configuracion.Almacen;
+using sw.Datos.Configuracion.Gastos;
 
 using sw.Datos.Clientes.Cliente;
 
@@ -40,6 +41,7 @@ using sw.Entidades.Administracion.PerfilesPer;
 
 using sw.Entidades.Configuracion.CatalogoSAT;
 using sw.Entidades.Configuracion.Almacen;
+using sw.Entidades.Configuracion.Gastos;
 
 using sw.Entidades.Clientes.Cliente;
 
@@ -94,6 +96,9 @@ namespace sw.Datos
         public DbSet<E_Marca> E_Marcas { get; set; }
         public DbSet<E_Proveedor> E_Proveedor { get; set; }
 
+        //MODULO GASTOS
+        public DbSet<E_Gasto> E_Gastos { get; set; }
+
         //MODULO CLIENTE
         public DbSet<E_Clientes> E_Clientes { get; set; }
 
@@ -146,6 +151,8 @@ namespace sw.Datos
             //MODULO CONFIGURACION_ALMACEN
             modelBuilder.ApplyConfiguration(new D_Marca());
             modelBuilder.ApplyConfiguration(new D_Proveedor());
+            //MODULO GASTOS
+            modelBuilder.ApplyConfiguration(new D_Gasto());
             //MODULO CLIENTE
             modelBuilder.ApplyConfiguration(new D_Clientes());
             //MODULO ALMACEN
---
+++ sw.Datos/Migrations/20240716020504_151724-001.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sw.Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class _151724001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conf_Gasto",
+                columns: table => new
+                {
+                    IdGasto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    E_CorporativoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conf_Gasto", x => x.IdGasto);
+                    table.ForeignKey(
+                        name: "FK_Conf_Gasto_Admin_Coorporativo_E_CorporativoId",
+                        column: x => x.E_CorporativoId,
+                        principalTable: "Admin_Coorporativo",
+                        principalColumn: "IdCoorporativo",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conf_Gasto_E_CorporativoId",
+                table: "Conf_Gasto",
+                column: "E_CorporativoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conf_Gasto");
+        }
+    }
+}
